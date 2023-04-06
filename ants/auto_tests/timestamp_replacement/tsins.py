082ce8c1100cc06c8320beebd3e291fe6aca6caf
meoree <cokoson@yandex.ru>
2023-04-06 13:24:04 +0300

    Улучшения в connection. В tsins добавлена функция get_rpi_settings_for_script. Продолжается работа над отправкой скрипта на rpi через sftp

--- ants/auto_tests/timestamp_replacement/tsins.py
+++ ants/auto_tests/timestamp_replacement/tsins.py
@@ -24,22 +24,51 @@ logging.basicConfig(
     handlers=[RichHandler()]
 )
 
-path_home = Path(Path.home(), "python", "auto_network_test_system")
+path_home = Path(Path.home(), 'python', 'auto_network_test_system')
 
 path_test = Path(path_home, 'ants', 'tests', 'timestamp_replacement')
 path_connection = Path(path_home, 'data', 'connection_data')
 path_commands = Path(path_home, 'ants', 'auto_tests', 'timestamp_replacement', 'commands')
-path_test_network = Path(path_home,'data', 'network_test_configs')
+path_test_network = Path(path_home, 'data', 'network_test_configs')
+
 
 def timestamp_test_start(tsins_device_dict, tsins_network_params):
     with open(f"{path_connection}/devices.yaml") as file:
-        devices  = yaml.safe_load(file)
+        devices = yaml.safe_load(file)
 
-    #configure_ssfp(devices, "ssfp1_commands.txt", tsins_device_dict["ssfp"][0])
-    #configure_ssfp(devices, "ssfp2_commands.txt", tsins_device_dict["ssfp"][1])
+    rpi_src = tsins_device_dict["rpi"][0]
+    rpi_dst = tsins_device_dict["rpi"][1]
 
-    mac_src = get_rpi_settings_for_script(devices, tsins_device_dict["rpi"][0])
-    mac_dst = get_rpi_settings_for_script(devices, tsins_device_dict["rpi"][1])
+    # configure_ssfp(devices, "ssfp1_commands.txt", tsins_device_dict["ssfp"][0])
+    # configure_ssfp(devices, "ssfp2_commands.txt", tsins_device_dict["ssfp"][1])
+    print(tsins_network_params)
+    # mac_src = get_rpi_settings_for_script(devices, rpi_src)
+    # mac_dst = get_rpi_settings_for_script(devices, rpi_dst)
+    ip_src = tsins_network_params[rpi_src]["ip"]
+    ip_dst = tsins_network_params[rpi_dst]["ip"]
+    intf_src = tsins_network_params[rpi_src]["interface"]
+    vlan_src = tsins_network_params[rpi_src]["vlan"]
+
+    print(ip_src, vlan_src, ip_dst, intf_src)
+    # 10.3.50.102 10.3.50.103 550 eth0
+    # все данные есть, далее отправим скрипт на rpi, потом на dst rpi включаем захват пакетов, потом отправим скрипт на src rpi
+
+    script_path = Path(path_test, 'pattern_ssfp.py')
+    send_script_to_rpi(devices, script_path, rpi_src)
+
+
+def send_script_to_rpi(devices, local_path, rpi):
+    try:
+        remote_path = "tests/"
+        with SFTPParamiko(**devices[rpi]) as sftp:
+            print(sftp.check_dir(remote_path))
+            sftp.mkdir(f"{remote_path}/tsins")
+           # sftp.put_file(local_path, remote_path)
+    except (FileNotFoundError, FileExistsError) as error:
+        logging.error(f"An error occurred while working with the file '{local_path}' - {error}")
+    except OSError as error:
+        logging.error(f"An error has occurred on the device {rpi} - {error}")
+
 
 def configure_ssfp(devices, file, ssfp):
     try:
@@ -48,36 +77,32 @@ def configure_ssfp(devices, file, ssfp):
         with BaseSSHParamiko(**devices[ssfp]) as ssh:
             output = ssh.send_shell_show_commands(ssfp1_commands, print_output=True)
         return output
-    except (FileNotFoundError,  FileExistsError) as error:
+    except (FileNotFoundError, FileExistsError) as error:
         logging.error(f"An error occurred while working with the file '{file}' - {error}")
     except OSError as error:
         logging.error(f"An error has occurred on the device {ssh.ip} - {error}")
+
 
 def get_rpi_settings_for_script(devices, rpi, vlan=550):
     try:
         with BaseSSHParamiko(**devices[rpi]) as ssh:
             output = ssh.send_exec_commands("ip a")
+            match = re.search(rf"\w+.{vlan}.+link/ether\s+(?P<mac>\S+)", output, re.DOTALL)
+            if match:
+                return match.group('mac')
+            else:
+                logging.error(f"The subinterface is not configured on the device {ssh.ip}")
     except socket.timeout as error:
-         logging.error(f"An error has occurred on the device - {error}")
-    match = re.search(rf"\w+.{vlan}.+link/ether\s+(?P<mac>\S+)", output, re.DOTALL)
-    if match:
-        return match.group('mac')
-    else:
-        logging.error(f"The subinterface is not configured on the device {ssh.ip}")
+        logging.error(f"An error has occurred on the device - {error}")
+
 
 if __name__ == "__main__":
     tsins_device_dict = {
         "ssfp": ["ssfp4", "ssfp8"],
-        "rpi" : ["rpi2", "rpi3"], 
+        "rpi": ["rpi2", "rpi3"],
     }
     print(Path.home())
-    #должен передавать ci-cd
+    # должен передавать ci-cd
     with open(f"{path_test_network}/network_params_for_test.yaml") as file:
         network_params = yaml.safe_load(file)
     timestamp_test_start(tsins_device_dict, network_params)
- 
-
-
-
-
-        
